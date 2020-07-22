5e46f2e1ff4ae32e691b448102cab3ff53c6ec97
MaestroSheik01 <67071158+MaestroSheik01@users.noreply.github.com>
2020-07-22 11:56:19 -0400

    Updated Player Movement
    Now runs L to R, flipping in direction, sprites centered.

---
+++ PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class PlayerMovement : MonoBehaviour
+{
+	//necessarry for animations and physics
+	private Rigidbody2D rb2D;
+	private Animator myAnimator;
+
+	private bool facingRight = true;
+
+	//variables to play with
+	public float speed = 2.0f;
+	public float horizMovement;
+
+	// Start is called before the first frame update
+	private void Start()
+	{
+		//define the gameobjects found on the player
+		rb2D = GetComponent<Rigidbody2D>();
+		myAnimator = GetComponent<Animator>();
+	}
+
+	// handles input for physics
+	private void Update()
+	{
+		//check direction given by player
+		horizMovement = Input.GetAxis("Horizontal");
+	}
+
+	//handles running the physics
+	private void FixedUpdate()
+	{
+		//move character left and right
+		rb2D.velocity = new Vector2(horizMovement * speed, rb2D.velocity.y);
+
+		myAnimator.SetFloat("speed", Mathf.Abs(horizMovement));
+
+		Flip(horizMovement);
+	}
+
+	//flipping function
+	private void Flip(float horizontal)
+	{
+		if (horizontal < 0 && facingRight || horizontal > 0 && !facingRight)
+		{
+			facingRight = !facingRight;
+
+			Vector3 theScale = transform.localScale;
+			theScale.x *= -1;
+			transform.localScale = theScale;
+		}
+	}
+}
